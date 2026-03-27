ae6a1cc9fd5c3184816b5738cf8e264a9bda088d
Shanaka Minindu <shanakaminindu@Shanakas-MacBook-Pro.local>
2026-03-27 17:51:02 +0530

    bug fix

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,10 +1,9 @@
-
-
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { PrismaClient } from "@/lib/generated/prisma";
+import { PrismaClient } from "@/lib/generated/prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+ 
 
 const adapter = new PrismaNeon({
   connectionString: process.env.DATABASE_URL!,
-})
+});
 
-export const prisma = new PrismaClient({ adapter })
+export const prisma = new PrismaClient({ adapter });
